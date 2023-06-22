c53a8968bf1c808221b303a4604dc1b10ea8ab74
akgusgh1234 <110758817+akgusgh1234@users.noreply.github.com>
2023-06-22 18:02:46 +0900

    Delete query.sql

--- query.sql
+++
@@ -1,5 +0,0 @@
-SELECT name,
-       round(( 6371 * acos( cos( radians(35.88445510796) ) * cos( radians(lat) ) * cos( radians(long) - radians(128.61420749660) ) + sin( radians(35.88445510796) ) * sin(radians(lat)) ) ),4)
-       AS distance
-FROM [dbo].[hospitalbedTBL]
-ORDER BY distance
