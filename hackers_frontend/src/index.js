204e5b8e9aadf767d7e17039f0c71d1e27fc2ec0
stellastudio <6uf3rw@naver.com>
2023-06-22 09:13:09 +0900

    EE

--- hackers_frontend/src/index.js
+++ hackers_frontend/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Routing from './Routed';
-import Main_page from './mainpage/mainpage';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
